3b5d6f219f54161bfe3516c9d9ca81aa3b3b1a8c
WyFly <12977083@qq.com>
2023-10-10 23:18:13 +0800

    移除链表元素

---
+++ Question/10.10/removeElements.c
@@ -0,0 +1,26 @@
+struct ListNode* removeElements(struct ListNode* head, int val){
+    if(head == NULL)
+        return NULL;
+    struct ListNode* cur = head;
+    struct ListNode* prev = NULL;
+    while(cur != NULL)
+    {
+        if(cur->val == val){
+            if(cur == head){
+                head = cur->next;
+                free(cur);
+                cur = head;
+            }
+            else{
+                prev->next = cur->next;
+                free(cur);
+                cur = prev->next;
+            } 
+        }
+        else{
+            prev = cur;
+            cur = cur->next;
+        }
+    }
+    return head;
+}
